67034143df1a92aeaea84423a96fff7925699d8d
Daina Slezaite <daina.slezaite@gmail.com>
2021-02-04 16:23:39 +0100

    Iteration 6 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
 import Header from './components/Header';
 import HomeMenu from './components/HomeMenu';
 import './App.css';
--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import './NewBeer.css';
 
 export default class NewBeer extends Component {
 
@@ -11,7 +13,8 @@ export default class NewBeer extends Component {
             first_brewed: '',
             brewers_tips: '',
             attenuation_level: 0,
-            contributed_by: ''
+            contributed_by: '',
+            redirectToBeers: false
         }
     }
 
@@ -22,25 +25,60 @@ export default class NewBeer extends Component {
         })
     }
 
+    handleSubmit = e => {
+        e.preventDefault();
+        fetch('https://ih-beers-api2.herokuapp.com/beers/new', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    name: this.state.name,
+                    tagline: this.state.tagline,
+                    description: this.state.description,
+                    first_brewed: this.state.first_brewed,
+                    brewers_tips: this.state.brewers_tips,
+                    attenuation_level: this.state.attenuation_level,
+                    contributed_by: this.state.contributed_by
+                })
+        })
+        .then(() => {
+            this.setState({
+                redirectToBeers: true
+            })
+        })
+    }
+
     render() {
-        return (
-            <form>
-                <label>Name</label>
-                <input type='text' value={this.state.name} name='name' onChange={this.handleInputChange} />
-                <label>Tagline</label>
-                <input type='text' value={this.state.tagline} name='tagline' onChange={this.handleInputChange} />
-                <label>Description</label>
-                <textarea type='text' value={this.state.description} name='description' onChange={this.handleInputChange}></textarea>
-                <label>First brewed</label>
-                <input type='text' value={this.state.firstBrewed} name='first_brewed' onChange={this.handleInputChange} />
-                <label>Brewers tips</label>
-                <input type='text' value={this.state.brewersTips} name='brewers_tips' onChange={this.handleInputChange} />
-                <label>Attenuation level</label>
-                <input type='number' value={this.state.attenuationLevel} name='attenuation_level' onChange={this.handleInputChange} />
-                <label>Contributed  by</label>
-                <input type='text' value={this.state.contributedBy} name='contributed_by' onChange={this.handleInputChange} />
-                <button type='submit'>Create</button>
-            </form>
-        )
+        if (this.state.redirectToBeers === true) {
+           return <Redirect to='/beers' />
+        } else {
+            return (
+                <form action={this.props.action} method={this.props.method} onSubmit={this.handleSubmit} className='new-beer'>
+                    <label>Name</label>
+                    <input type='text' value={this.state.name} name='name' onChange={this.handleInputChange} />
+                    <label>Tagline</label>
+                    <input type='text' value={this.state.tagline} name='tagline' onChange={this.handleInputChange} />
+                    <label>Description</label>
+                    <textarea type='text' value={this.state.description} name='description' onChange={this.handleInputChange}></textarea>
+                    <label>First brewed</label>
+                    <input type='text' value={this.state.first_brewed} name='first_brewed' onChange={this.handleInputChange} />
+                    <label>Brewers tips</label>
+                    <input type='text' value={this.state.brewers_tips} name='brewers_tips' onChange={this.handleInputChange} />
+                    <label>Attenuation level</label>
+                    <input type='number' value={this.state.attenuation_level} name='attenuation_level' onChange={this.handleInputChange} />
+                    <label>Contributed  by</label>
+                    <input type='text' value={this.state.contributed_by} name='contributed_by' onChange={this.handleInputChange} />
+                    <button type='submit'>Create</button>
+                </form>
+            )
+        }
     }
 }
+
+NewBeer.defaultProps = {
+    action: 'https://ih-beers-api2.herokuapp.com/beers/new',
+    method: 'POST'
+};
